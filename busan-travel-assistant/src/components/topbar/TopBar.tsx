d7caa42f7800334b951af86740d52f0d590fff63
jasonlee1119 <147239482+jasonlee1119@users.noreply.github.com>
2025-12-25 20:52:00 +0800

    feat: add title and icon

--- busan-travel-assistant/src/components/topbar/TopBar.tsx
+++ busan-travel-assistant/src/components/topbar/TopBar.tsx
@@ -1,6 +1,8 @@
-import { AppBar, Box, Toolbar } from '@mui/material';
+import { AppBar, Box, Stack, Toolbar } from '@mui/material';
 import { NavigationDrawer } from './NavigationDrawer';
 import { ThemeSelector } from './ThemeSelector';
+import { Link } from 'react-router';
+import icon from '../../assets/south-korea-svgrepo-com.svg';
 
 export function TopBar() {
   return (
@@ -12,6 +14,14 @@ export function TopBar() {
       >
         <Toolbar>
           <NavigationDrawer />
+          <Link to="/" className="text-decoration-none">
+            <Stack direction="row" alignItems="center" spacing={1}>
+              <img src={icon} alt="My Icon" width={30} height={30} />
+              <Box component="span" sx={{ mr: 2 }}>
+                釜山旅遊小幫手
+              </Box>
+            </Stack>
+          </Link>
           <Box className="flex-grow" />
           <Box>
             <ThemeSelector />
