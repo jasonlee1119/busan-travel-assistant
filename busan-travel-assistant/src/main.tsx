d41d49541b6f5d568586c8fa9df56b29bfc798cb
jasonlee1119 <147239482+jasonlee1119@users.noreply.github.com>
2025-12-25 13:55:35 +0800

    feat: add root settings

--- busan-travel-assistant/src/main.tsx
+++ busan-travel-assistant/src/main.tsx
@@ -1,10 +1,71 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+import {
+  createTheme,
+  GlobalStyles,
+  Icon,
+  IconButton,
+  StyledEngineProvider,
+  ThemeProvider,
+} from '@mui/material';
+import { amber, lightBlue } from '@mui/material/colors';
+import { closeSnackbar, SnackbarProvider } from 'notistack';
+
+const theme = createTheme({
+  cssVariables: true,
+  colorSchemes: {
+    light: {
+      palette: {
+        primary: {
+          main: amber[700],
+        },
+        secondary: {
+          main: lightBlue[700],
+        },
+      },
+    },
+    dark: {
+      palette: {
+        primary: {
+          main: amber[500],
+        },
+        secondary: {
+          main: lightBlue[500],
+        },
+      },
+    },
+  },
+  defaultColorScheme: 'light',
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <StyledEngineProvider enableCssLayer>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles styles="@layer base, mui, utilities;" />
+        <SnackbarProvider
+          maxSnack={3}
+          anchorOrigin={{
+            vertical: 'top',
+            horizontal: 'right',
+          }}
+          action={(snackbarId) => (
+            <IconButton
+              size="small"
+              aria-label="close"
+              color="inherit"
+              onClick={() => closeSnackbar(snackbarId)}
+            >
+              <Icon fontSize="small">Close</Icon>
+            </IconButton>
+          )}
+        >
+          <App />
+        </SnackbarProvider>
+      </ThemeProvider>
+    </StyledEngineProvider>
+  </StrictMode>
+);
+
